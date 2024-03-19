c467d9e672634f32f82900f73000ac4a722326b3
ZweiHenky <ronnhy7@gmail.com>
2024-03-18 18:40:27 -0600

    config tailwind

---
+++ app/utils/dynamic/tailwind.config.js
@@ -0,0 +1,13 @@
+
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          background: '#FAFAFA',
+          primary: '#3C91E6',
+        }
+      }
+    }
+  }
+
+console.log('hola');
