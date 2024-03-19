ff7f3c7de3fd1ea2a7192539bc28b2488d1a3acf
ZweiHenky <ronnhy7@gmail.com>
2024-03-18 18:39:05 -0600

    create function profile

--- app/controllers/DynamicController.php
+++ app/controllers/DynamicController.php
@@ -5,9 +5,30 @@
 class DynamicController {
     
     public function home() {
-        // Lógica para la página de inicio estática
+        // Lógica para la página de inicio del usuario
+
+
+        // 1.- buscar todas las publicaciones con paginacion
+
+        // 2.- buscar por titulo y categoria con paginacion 
+
 
         include 'app/views/dynamic/home.php';
+    }
+
+    public function profile(){
+        // Lógica para la página de perfil del usuario
+
+        // 1.- Mostrar los datos del usuario con la variable $_SESSION['usuario']
+
+        // 2.- mostrar las publicaciones de la tabla notificaciones con los datos que estan en la tabla de ejemplo con paginacion
+
+        // 3.- Mostrar las reseñas en la seccion de reseñas con paginacion
+
+        // 4.- Mostrar las publicaciones guardadas del usuario en la seccion de guardados
+
+
+        include 'app/views/dynamic/profile.php';
     }
 
     // Otros métodos para páginas estáticas según sea necesario
