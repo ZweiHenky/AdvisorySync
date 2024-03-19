bf3fbc4d60257601322e163d80d9d97f19bc7adb
ZweiHenky <ronnhy7@gmail.com>
2024-03-18 18:40:47 -0600

    update view home dynamic

--- app/views/dynamic/home.php
+++ app/views/dynamic/home.php
@@ -5,48 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src="../app/utils/dinamyc/tailwind.config.js"></script>
+    <script src="../app/utils/dynamic/tailwind.config.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.7.3/dist/full.min.css" rel="stylesheet" type="text/css" />
 
 </head>
 <body>
 
-    <div class="backNav closeNav w-full bg-[#00000060] absolute h-dvh top-0 left-0 z-20 transition-transform -translate-x-full">
-    </div>
-
-    <aside class=" min-w-[70%] max-w-[80%] h-dvh bg-white p-3 absolute z-30 transition-transform -translate-x-full">
-        
-        <section class='flex justify-between items-center'>
-            <h1 class='text-3xl'>AdvisorySync</h1>
-            <img src="../app/assets/icons/close.svg" class='closeNav w-[15px] h-auto' alt="" srcset="">
-        </section>
-
-        <nav class='mt-10'>
-            
-            <ul class='flex flex-col gap-10'>
-                <li class='flex gap-2 items-center'>
-                    <img class='w-[20px] h-auto' src="../app/assets/icons/home.svg" alt="" > 
-                    <p>Inicio</p>
-                </li>
-                <li class='flex gap-2  items-center'>
-                    <img class='w-[20px] h-auto' src="../app/assets/icons/user.svg" alt="" >
-                    <p>Perfil</p>
-                </li>
-                <li class='flex gap-2 items-center'>
-                    <img class='w-[20px] h-auto' src="../app/assets/icons/setting.svg" alt="" >
-                    <p>Configuracion</p>
-                </li>
-                <li class='flex gap-2 items-center'>
-                    <img class='w-[20px] h-auto' src="../app/assets/icons/logOut.svg" alt="" >
-                    <p>Cerrar Sesion</p>
-                </li>
-            </ul>
-
-        </nav>
-
-        <button class='mt-12 bg-primary text-white w-full p-2 rounded'>Crear Nueva Asesoria</button>
-
-    </aside>
+    <?php
+        include_once('app/views/templates/dynamic/aside.php')
+    ?>
 
     <header class='bg-background border-b flex w-full justify-between p-5 items-center'>
         <div>
@@ -56,11 +23,11 @@
     </header>
 
     <main>
-        <div class='mt-5 w-full p-5 bg-background'>
+        <div class='mt-5 w-full p-5 '>
             <form class='flex *:rounded gap-5 flex-col' action="">
                 <div class=' flex *:p-2 gap-5'>
-                    <input class='basis-1/2' type="text" name="" id="" placeholder='Buscar Publicacion'>
-                    <select name="" id="" class='basis-1/2'>
+                    <input class='basis-1/2 bg-background rounded border' type="text" name="" id="" placeholder='Buscar Publicacion'>
+                    <select name="" id="" class='bg-background text-gray-400 border rounded basis-1/2'>
                         <option value="">categorias</option>
                     </select>
                 </div>
@@ -70,7 +37,7 @@
 
         <h2 class='text-center m-5 text-3xl'>Publicaciones</h2>
 
-        <section class='bg-background min-w-[90%] max-w-[90%] h-[500px] *:p-4 mb-5 mx-auto relative border border-gray-200 rounded-md shadow-md '>
+        <section class=' min-w-[90%] max-w-[90%] h-[500px] *:p-4 mb-5 mx-auto relative border border-gray-300 rounded-md shadow-md '>
             <p class='absolute right-0 top-0 p-4 text-xl'>usuario</p>
             <h3 class='text-xl border-b'>Titulo</h3>
             <div class='mt-4'>
@@ -79,33 +46,23 @@
             <div class='mt-4'>
                 <p class='text-balance truncate ...'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, nobis? Neque minus magnam sint quibusdam nostrum velit officiis et earum rerum est? Doloribus inventore deleniti saepe optio repellat atque quam.</p>
             </div>
-            <button >Seleccionar Horario</button>
+            <div>
+                <h4>Selecciona un horario</h4>
+                <select class='mt-2 p-2 w-full rounded bg-background text-gray-400 border ' name="" id="">
+                    <option value="">Opciones</option>
+                    <option value="">Lunes: 2:00 pm - 4:00 pm</option>
+                    <option value="">Miercoles: 2:00 pm - 4:00 pm</option>
+                    <option value="">Jueves: 2:00 pm - 4:00 pm</option>
+                </select>
+            </div>
             
             <p class='absolute left-0 bottom-0 p-4 '>dias</p>
             <div>
-                <button class='absolute right-0 bottom-0 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm p-3 m-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900'>Aceptar</button>
+                <button class='absolute right-0 bottom-0 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm py-3 px-5 m-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900'>Aceptar</button>
             </div>
         </section>
     </main>
 
-    <script>
-        const btnClose = document.querySelectorAll('.closeNav')
-        const back = document.querySelector('.backNav')
-        const aside = document.querySelector('aside')
-
-        const openMenu = document.querySelector('.openMenu')
-        
-        openMenu.addEventListener('click', ()=>{
-            aside.classList.remove('-translate-x-full')
-            back.classList.remove('-translate-x-full')
-        })
-
-        btnClose.forEach(btn =>{
-            btn.addEventListener('click', ()=>{
-                aside.classList.add('-translate-x-full')
-                back.classList.add('-translate-x-full')
-            })
-        })
-    </script>
+    <script src='../app/utils/dynamic/aside.js'></script>
 </body>
 </html>
