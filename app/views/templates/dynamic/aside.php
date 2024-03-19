78936829fd31c3f2e225ac1c0ee9941bf6a784a5
ZweiHenky <ronnhy7@gmail.com>
2024-03-18 18:41:32 -0600

    split aside of view home dynamic

---
+++ app/views/templates/dynamic/aside.php
@@ -0,0 +1,40 @@
+<div class="backNav closeNav w-full bg-[#00000020] absolute h-dvh top-0 left-0 z-20 transition-transform -translate-x-full">
+</div>
+
+<aside class=" min-w-[70%] max-w-[80%] h-dvh bg-white p-3 absolute z-30 transition-transform -translate-x-full">
+    
+    <section class='flex justify-between items-center'>
+        <h1 class='text-3xl'>AdvisorySync</h1>
+        <img src="../app/assets/icons/close.svg" class='closeNav w-[15px] h-auto' alt="" srcset="">
+    </section>
+
+    <nav class='mt-10'>
+        
+        <ul class='flex flex-col gap-10'>
+            <a href="home">
+                <li class='flex gap-2 items-center'>
+                    <img class='w-[20px] h-auto' src="../app/assets/icons/home.svg" alt="" > 
+                    <p>Inicio</p>
+                </li>
+            </a>
+            <a href="profile">
+                <li class='flex gap-2  items-center'>
+                    <img class='w-[20px] h-auto' src="../app/assets/icons/user.svg" alt="" >
+                    <p>Perfil</p>
+                </li>
+            </a>
+            <li class='flex gap-2 items-center'>
+                <img class='w-[20px] h-auto' src="../app/assets/icons/setting.svg" alt="" >
+                <p>Configuracion</p>
+            </li>
+            <li class='flex gap-2 items-center'>
+                <img class='w-[20px] h-auto' src="../app/assets/icons/logOut.svg" alt="" >
+                <p>Cerrar Sesion</p>
+            </li>
+        </ul>
+
+    </nav>
+
+        <button class='mt-12 bg-primary text-white w-full p-2 rounded'>Crear Nueva Asesoria</button>
+
+    </aside>
